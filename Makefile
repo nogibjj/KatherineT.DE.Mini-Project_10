295ca7317640be8e28be5cbb0383b685eddd428b
katherinetian540 <143833511+katherinetian540@users.noreply.github.com>
2023-11-10 00:56:59 +0000

    1st

--- Makefile
+++ Makefile
@@ -3,13 +3,16 @@ install:
 		pip install -r requirements.txt
 
 test:
-	python -m pytest -vv --cov=main  test_*.py
+	python -m pytest -vv -cov=mylib test_*.py
 
 format:	
 	black *.py 
 
 lint:
-	ruff check *.py 
+	#disable comment to test speed
+	#pylint --disable=R,C --ignore-patterns=test_.*?py *.py mylib/*.py
+	#ruff linting is 10-100X faster than pylint
+	ruff check *.py mylib/*.py
 
 container-lint:
 	docker run --rm -i hadolint/hadolint < Dockerfile
@@ -22,15 +25,12 @@ deploy:
 all: install lint test format deploy
 
 generate_and_push:
-	# Create the markdown file (assuming it's generated from the plot)
-	python test_main.py  # Replace with the actual command to generate the markdown
-
 	# Add, commit, and push the generated files to GitHub
 	@if [ -n "$$(git status --porcelain)" ]; then \
 		git config --local user.email "action@github.com"; \
 		git config --local user.name "GitHub Action"; \
-		git add congress.png congress_summary.md; \
-		git commit -m "Add generated plot and report"; \
+		git add .; \
+		git commit -m "Add output log"; \
 		git push; \
 	else \
 		echo "No changes to commit. Skipping commit and push."; \
--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 """
-Main function goes here
+Main goes here
 """
+from mylib.lib import (
+    extract,
+    load_data,
+    describe,
+    query,
+    example_transform,
+    start_spark,
+    end_spark,
+)
 
-# import pandas and pyplot
-import matplotlib.pyplot as plt
+
+def main():
+    # extract data
+    extract()
+    # start spark session
+    spark = start_spark("Birth")
+    # load data into dataframe
+    df = load_data(spark)
+    # example metrics
+    describe(df)
+    # query
+    query(
+        spark,
+        df,
+        "SELECT YEAR, Month, Births FROM Birth where Births>8000",
+        "Birth",
+    )
+    # example transform
+    example_transform(df)
+    # end spark session
+    end_spark(spark)
 
 
-# define summary statistics function
-def sum_stats(data):
-    return data.describe()
-
-
-# define visualization function
-def visualization(data):
-    plt.hist(data["calories"], bins=10, color="purple")
-    plt.xlabel("Calories")
-    plt.ylabel("Frequency")
-    plt.title("Cereal Calories Histogram")
-    plt.show()
-    plt.savefig("cereal.png")
-    return
-
-def generate_markdown(data):
-    """generate an md file with outputs"""
-    markdown_table = sum_stats(data)
-    markdown_table = str(markdown_table)
-
-    # Write the markdown table to a file
-    with open("output.md", "w", encoding="utf-8") as file:
-        file.write("Summary:\n")
-        file.write(markdown_table)
-        file.write("\n\n")  # Add a new line
-        file.write("![cereal](cereal.png)\n")
+if __name__ == "__main__":
+    main()
